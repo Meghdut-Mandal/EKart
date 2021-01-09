a05792eab281118a705ed3c3e5f2c6d16f05a579
Meghdut Mandal <meghdut.windows@gmail.com>
2021-01-09 10:05:18 +0530

    Added readItem for IOProvider

--- src/controllers/IOProvider.kt
+++ src/controllers/IOProvider.kt
@@ -6,3 +6,24 @@ interface IOProvider {
     fun println(line: String)
     fun println(obj: Any)
 }
+
+/*
+ Reads an item from the IOProvider preventing Empty Values from the User
+ */
+fun IOProvider.readItem(name: String, empty: Boolean = false): String {
+    var input: String
+    do {
+        this.println("Enter the $name")
+        input = this.readLine()
+
+        if (!empty && input.isBlank()) {
+            this.println("$name can't be empty Please try again!")
+            continue
+        } else {
+            return input
+        }
+
+    } while (input.isBlank() && !empty)
+
+    return input
+}
