cbd8be1448f0e88d517c0f99e86505116a213fe3
Meghdut Mandal <meghdut.windows@gmail.com>
2021-01-09 10:50:24 +0530

    Changed id to emailId

--- src/storage/UserDaoImpl.kt
+++ src/storage/UserDaoImpl.kt
@@ -7,18 +7,18 @@ class UserDaoImpl : UsersDao {
     private val users = mutableListOf<User>()
 
     override fun getUserById(id: String): User {
-        if (isValidID(id)) {
+        if (isValidEmailId(id)) {
             return users.find { it.id == id }!!
         }
         throw IllegalArgumentException("The given id is Invalid")
     }
 
-    override fun isValidID(id: String): Boolean {
-        return users.any { it.id == id }
+    override fun isValidEmailId(emailId: String): Boolean {
+        return users.any { it.email == emailId }
     }
 
     override fun addUser(user: User) {
-        if (isValidID(user.id)) {
+        if (isValidEmailId(user.id)) {
             throw IllegalArgumentException("An User with the given ID exists! ")
         }
         users.add(user)
