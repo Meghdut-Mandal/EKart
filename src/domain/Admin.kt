6eb8ff5d84967b3a19d568e1e6d0d38ad404a9c0
Meghdut Mandal <meghdut.windows@gmail.com>
2021-01-09 10:16:25 +0530

    Fixed Type Error in Admin Object

--- src/domain/Admin.kt
+++ src/domain/Admin.kt
@@ -4,6 +4,6 @@ data class Admin(
     val id: String,
     val name: String,
     val email: String,
-    val passwordHash: String,
-    val isOwner: String
+    val passwordHash: Int,
+    val isOwner: Boolean
 )
