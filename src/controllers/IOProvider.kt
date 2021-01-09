d3463ae111af684c8e440165477b46fb017254fe
Meghdut Mandal <meghdut.windows@gmail.com>
2021-01-09 18:48:34 +0530

    Added readIntItem for the IOProvider

--- src/controllers/IOProvider.kt
+++ src/controllers/IOProvider.kt
@@ -29,7 +29,7 @@ fun IOProvider.readItem(name: String, empty: Boolean = false): String {
 }
 
 /*
- Read a proper Double value properly from the provider
+ Read a proper Double value properly from the provider.Prompt if wrong  Value entered
  */
 fun IOProvider.readDoubleItem(name: String, isValid: (Double) -> Boolean = { true }): Double {
     val input = 0.0
@@ -39,11 +39,31 @@ fun IOProvider.readDoubleItem(name: String, isValid: (Double) -> Boolean = { tru
         if (str == null || isValid(str)) {
             println("Enter a valid $name")
             continue
-        }else{
+        } else {
             return str
         }
 
     } while (!isValid(input))
 
     return 0.0
+}
+
+/*
+ Read an integer properly. And Prompt if wrong value entered.
+ */
+fun IOProvider.readIntItem(name: String, isValid: (Int) -> Boolean = { true }): Int {
+    val input = 1
+    do {
+        val str = readItem(name).toIntOrNull()
+
+        if (str == null || isValid(str)) {
+            println("Enter a valid $name")
+            continue
+        } else {
+            return str
+        }
+
+    } while (!isValid(input))
+
+    return 0
 }
